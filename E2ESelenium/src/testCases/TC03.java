613f7420193d53291da4e644a0ec42926cf0c618
Dharam <dharam.iyer@gmail.com>
2018-02-19 14:48:26 -0800

    First commit of framework related tests

---
+++ E2ESelenium/src/testCases/TC03.java
@@ -0,0 +1,18 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC03 {
+	@Test
+	public void tC1() {
+		System.setProperty("webdriver.chrome.driver", "/Users/dharam/MyWork/E2ESelenium/Driver/chromedriver");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("Hello");
+		driver.findElement(By.id("pass")).sendKeys("howdy");
+		driver.close();
+		driver.quit();
+	}
+}
